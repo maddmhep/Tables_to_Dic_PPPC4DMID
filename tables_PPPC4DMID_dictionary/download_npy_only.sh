e588ae3c3064a2704983cd5168b9af6f67c82042
Chiara Arina <chiara.arina@uclouvain.be>
2018-02-27 16:56:38 +0100

    adding new dir

--- tables_PPPC4DMID_dictionary/download_npy_only.sh
+++ tables_PPPC4DMID_dictionary/download_npy_only.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 tar -zcvf tables_npy_pppc4dmid.tar.gz PPPC_Tables_noEW.npy PPPC_Tables_EW.npy PPPC_Tables_epEarth_NFW.npy PPPC_Tables_epEarth_Ein.npy PPPC_Tables_epEarth_Iso.npy PPPC_Tables_epEarth_Moo.npy
+
